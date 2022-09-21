98103cff081b117ae257f0be2738a55ecfebd55d
Wiktoria Pieczykolan <20085118@mail.setu.ie>
2022-09-21 16:46:34 +0100

    Dynamic properties.

--- fundamentals.js
+++ fundamentals.js
@@ -9,6 +9,7 @@
   };
   
   console.log(aCar.owner + " drives a " + aCar.type.make);
+
   console.log(
     "Reg. = " +
       aCar.registration.year +
@@ -16,4 +17,20 @@
       aCar.registration.county +
       "-" +
       aCar.registration.number
+  );
+  
+  aCar.milage = 10000
+  aCar.color = {
+    exterior: "red",
+    interior: { texture: "leather", shade: "cream" }
+  }
+  
+  console.log(
+    "It is a " +
+      aCar.color.exterior +
+      " car, " +
+      aCar.milage +
+      " milage, with " +
+      aCar.color.interior.texture +
+      " interior."
   );
